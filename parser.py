bc5d96fb456d6a6132f8577c8bc198e4ec1adb61
Zhongwei Yao <ashi08104@gmail.com>
2014-12-25 10:07:33 +0800

    FB.

---
+++ parser.py
@@ -0,0 +1,89 @@
+def get_paired_parenthes_pos(s, left_paren_pos):
+    """Get left_paren_pos's paired parenthes in s.
+    If no parenthes pos found or unpair parenthes included, an error will be
+    thrown.
+    """
+    assert(s)
+    assert(s[left_paren_pos] == '(')
+
+    def get_paired_parenthes_pos_help(start_pos):
+        try:
+            next_p = start_pos + 1
+            # If () case
+            if s[next_p] == ')':
+                return next_p
+            # if ((... case
+            elif s[next_p] == '(':
+                return get_paired_parenthes_pos_help(
+                    get_paired_parenthes_pos_help(next_p))
+            # if (abc... case
+            else:
+                return get_paired_parenthes_pos_help(next_p)
+        except IndexError:
+            raise Exception("Unmatched parenthes found.")
+    
+    return get_paired_parenthes_pos_help(left_paren_pos)
+
+def str2tk(s):
+    """break the string into tokens
+    e.g. "(33+2)" -> ["(", "33", "+", "2", ")"]"""
+
+    if not s:
+        return []
+    elif s[0].isdigit():
+        dig_len = 1
+        while dig_len < len(s) and s[dig_len].isdigit():
+            dig_len += 1
+        return [s[:dig_len]] + str2tk(s[dig_len:])
+    elif s[0].isspace():
+        return str2tk(s[1:])
+    else:
+        return [s[0]] + str2tk(s[1:])
+
+class AstNode(object):
+    def __init__(self, token):
+        self.token = token
+        self.left = None
+        self.right = None
+
+    def __str__(self):
+        left = self.left
+        right = self.right
+        if not left:
+            left = "N"
+        if not right:
+            right = "N"
+        return "[{}:{} {}]".format(self.token,
+                                   left,
+                                   right)
+
+def parser_add(exps):
+    """Parse following grammar and return the AST:
+       E->n
+       E->(E+E)
+    Input is list of tokens: ["(", "33", "+", "2", ")"]
+    """
+    if exps[0].isdigit():
+        # case of E->n
+        return AstNode(exps[0])
+    elif exps[0] == "(":
+        # case of E->(E+E)
+        # Look forward one element.
+        ast = AstNode("+")
+        if exps[1] == "(":
+            pos = get_paired_parenthes_pos(exps, 1)
+            ast.left = parser_add(exps[1:pos + 1])
+            # exclude last ")"
+            ast.right = parser_add(exps[pos + 2:-1])
+            return ast
+        else:
+            # is digit
+            ast.left = AstNode(exps[1])
+            ast.right = parser_add(exps[3:])
+            return ast
+
+def parser2ast(s):
+    return parser_add(str2tk(s))
+
+if __name__ == "__main__":
+    print str2tk("(  1+3) ")
---
+++ test.py
@@ -0,0 +1,106 @@
+import unittest
+from parser import get_paired_parenthes_pos, str2tk, parser2ast
+
+class TestPairedParenthes(unittest.TestCase):
+    def setUp(self):
+        pass
+        
+    def test_rightp(self):
+        self.assertEqual(
+            get_paired_parenthes_pos("()", 0),
+            1)
+
+    def test_withcontent1(self):
+        self.assertEqual(
+            get_paired_parenthes_pos("(a)", 0),
+            2)
+
+    def test_withcontent2(self):
+        self.assertEqual(
+            get_paired_parenthes_pos("(ab)", 0),
+            3)
+
+    def test_withmoreparen1(self):
+        self.assertEqual(
+            get_paired_parenthes_pos("(())", 0),
+            3)
+
+    def test_withmoreparen2(self):
+        self.assertEqual(
+            get_paired_parenthes_pos("(())", 1),
+            2)
+
+    def test_withmoreparen3(self):
+        self.assertEqual(
+            get_paired_parenthes_pos("(0a(3)(dk(dd)((dd))))ab(", 0),
+            20)
+
+    def test_witherror1(self):
+        with self.assertRaises(Exception):
+            get_paired_parenthes_pos("(a", 0)
+
+    def test_witherror2(self):
+        with self.assertRaises(Exception):
+            get_paired_parenthes_pos("(a(ab)", 0)
+
+    def test_with_noerror3(self):
+        self.assertEqual(
+            get_paired_parenthes_pos("(a(ab))(", 0),
+            6)
+
+class TestStr2tk(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_none(self):
+        self.assertEqual(
+            str2tk(""),
+            [])
+        
+    def test_singledigit(self):
+        self.assertEqual(
+            str2tk("1"),
+            ["1"])
+
+    def test_whitepace(self):
+        self.assertEqual(
+            str2tk("(  1+3) "),
+            ['(', '1', '+', '3', ')'])
+
+    def test_multdigit(self):
+        self.assertEqual(
+            str2tk("11"),
+            ["11"])
+        
+    def test_others(self):
+        self.assertEqual(
+            str2tk("(11+2)"),
+            ["(", "11", "+", "2", ")"])
+
+class TestParser(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_n(self):
+        self.assertEqual(str(parser2ast("3")),
+                         "[3:N N]")
+
+    def test_e1(self):
+        self.assertEqual(str(parser2ast("(33+2)")),
+                         "[+:[33:N N] [2:N N]]")
+
+    def test_e2(self):
+        self.assertEqual(str(parser2ast("(33+(2+1))")),
+                         "[+:[33:N N] [+:[2:N N] [1:N N]]]")
+
+    def test_e3(self):
+        self.assertEqual(str(parser2ast("((3+3)+2)")),
+                         "[+:[+:[3:N N] [3:N N]] [2:N N]]")
+
+    def test_e3(self):
+        self.assertEqual(str(parser2ast("((1+((2+3)+3))+((((4+5)+(6+7))+8)+9))")),
+                         "[+:[+:[1:N N] [+:[+:[2:N N] [3:N N]] [3:N N]]] [+:[+:[+:[+:[4:N N] [5:N N]] [+:[6:N N] [7:N N]]] [8:N N]] [9:N N]]]")
+        
+
+if __name__ == "__main__":
+    unittest.main()
